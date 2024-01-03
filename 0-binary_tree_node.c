4d3a29c46938d4be84ffcc40592a654055a0b3d5
Sine <ssnngcongo@gmail.com>
2024-01-03 06:14:10 -0800

    updated script to be betty compliant

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - function that creates a binary tree node
- * parent: pointer to the parent node of the node to create
- * value: value to put in the new node
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
  * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
